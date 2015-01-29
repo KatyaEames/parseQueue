b889b30ca34577276df53ce54a6befefb558dc59
KatyaEames <katya.eames@gmail.com>
2015-01-29 15:19:22 -0700

    index

--- js/defaultHeaders.js
+++ js/defaultHeaders.js
@@ -3,7 +3,7 @@ var app = angular.module('parseQ');
 app.factory('httpRequestInterceptor', function () {
   return {
     request: function (config) {
-      config.headers = {'X-Parse-Application-Id': 'INSERT-YOUR-APPLICATION-ID', 'X-Parse-REST-API-Key': 'INSERT-YOUR-REST-API-KEY'}
+      config.headers = {'X-Parse-Application-Id': 'MO5a3svnAFW6V6FL88IJSOrJaixQXHlaIvAkdD55', 'X-Parse-REST-API-Key': 'SHPQc11mbIrA5aYibN7ZP7Axf9QoczWagwgJDIm8'}
       return config;
     }
   };
